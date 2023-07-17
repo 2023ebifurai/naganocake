b220153593c950366418b2090d2e6f71bb5d51b9
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-17 04:04:22 +0000

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  #devise_for :admins  
   
   namespace :admin do
-   resources :items 
+    resources :products, only: [:new, :create, :index, :show, :edit, :update]
   end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
