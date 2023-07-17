8ee8269f35c9d69e347a18221890fb9e19d1a03e
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-17 04:38:02 +0000

    godevelop

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  #devise_for :admins  
-  
+  #devise_for :admins
+
   namespace :admin do
     resources :products, only: [:new, :create, :index, :show, :edit, :update]
   end
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
